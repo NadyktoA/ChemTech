ff87cb85aa681d767aed79b5f40ee5c27756f9b9
Andrey Nadykto <artemka.nadykto@gmail.com>
2024-09-05 11:26:20 +0700

    Try to understand a commit automatically or not accepted on GitHub

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from PyQt5.QtWidgets import QApplication
 
 from work_sheet_window import WorkSheet
 
-print("testing")
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     work_sheet = WorkSheet()
