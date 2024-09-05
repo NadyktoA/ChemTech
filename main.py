35d87feeeef6bf5c4d898cf82074617153ed5d4f
Andrey Nadykto <artemka.nadykto@gmail.com>
2024-09-05 11:09:29 +0700

    Test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PyQt5.QtWidgets import QApplication
 
 from work_sheet_window import WorkSheet
 
+print("testing")
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     work_sheet = WorkSheet()
